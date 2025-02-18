2499feb7fe07c0c7f04787baf10e3d58c6f3115c
nowhereOnce <alejandro.aguilar.fi02@gmil.com>
2025-02-17 18:42:47 -0600

    Refactor main application to integrate Spotipy and Spotify OAuth with Redis caching and remove deprecated authentication routes. This only works for a single user since the cache_handler is global and is being overwritten every time a user logs in. I'll try to make it so many sessions can be supported in the future.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,61 @@
-from fastapi import FastAPI
-from app.routes import auth, playlists
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.responses import RedirectResponse
+from fastapi.security import HTTPBearer
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy.cache_handler import RedisCacheHandler
+import redis
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Configuración de Spotify
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
+scope = "user-library-read"
+
+# Configuración de Redis
+redis_client = redis.Redis(host="localhost", port=6379, db=0)
+
+cache_handler = RedisCacheHandler(redis_client)
+
+# Inicializar FastAPI
+app = FastAPI()
+security = HTTPBearer()
+
+# OAuth de Spotify
+sp_oauth = SpotifyOAuth(
+    client_id=SPOTIFY_CLIENT_ID,
+    client_secret=SPOTIFY_CLIENT_SECRET,
+    redirect_uri=SPOTIFY_REDIRECT_URI,
+    scope=scope,
+    cache_handler=cache_handler,
+    show_dialog=True,
+)
+
+sp = Spotify(auth_manager=sp_oauth)
 
 app = FastAPI()
 
-app.include_router(auth.router)
-app.include_router(playlists.router)
-
 @app.get("/")
 def read_root():
-    return {"Message": "Welcome to Statify!"}
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return RedirectResponse(auth_url)
+    return RedirectResponse("/get_playlists")
+
+@app.get("/callback")
+def callback(code: str):
+    token_info = sp_oauth.get_access_token(code)
+    cache_handler.save_token_to_cache(token_info)
+    return RedirectResponse("/get_playlists")
+
+@app.get("/get_playlists")
+def get_playlists():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        return RedirectResponse("/")
+    playlists = sp.current_user_playlists()
+    print(playlists)
+    return playlists

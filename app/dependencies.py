159e7e7ca3321fb9244a03d6996e783b1396a3f9
nowhereOnce <alejandro.aguilar.fi02@gmail.com>
2025-07-18 16:41:19 -0600

    Redis instance is no longer local, now runs in the cloud via URL and other parameters in the .env file

--- app/dependencies.py
+++ app/dependencies.py
@@ -8,12 +8,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# spotify connection
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
 SCOPE = "user-library-read, user-top-read"
 
-redis_client = redis.Redis(host="localhost", port=6379, db=0)
+# Redis-cloud config (local otherwise)
+REDIS_URL = os.getenv("REDIS_URL")  
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")  
+REDIS_PORT = os.getenv("REDIS_PORT", 6379)  
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD") 
+REDIS_DB = os.getenv("REDIS_DB", 0) 
+
+if REDIS_URL:
+    redis_client = redis.from_url(REDIS_URL)
+else:
+    redis_client = redis.Redis(
+        host=REDIS_HOST,
+        port=int(REDIS_PORT),
+        password=REDIS_PASSWORD,
+        db=int(REDIS_DB),
+        decode_responses=True,
+        ssl=True,
+    )
 
 def get_spotify_user_id(request: Request) -> str:
     user_id = request.cookies.get("spotify_user_id")
